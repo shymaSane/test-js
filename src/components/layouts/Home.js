bbf7d7c2f587ac7a08ca2f3639f859115148c1dd
shymaSane <shymaSane1@gmail.com>
2019-02-05 15:54:09 +0200

    basic homepage

--- src/components/layouts/Home.js
+++ src/components/layouts/Home.js
@@ -6,10 +6,15 @@ function Home() {
   return (
     <div className="container">
       <div className="cover-text">
-        <h4>Inspire your data</h4>
-        <h1>Welcome to ShowTables</h1>
-        <h3>Start with us</h3>
-      
+        <div className="text-container">
+            <h4 className="text1">Inspire your data</h4>
+            <h1 className="text2"><span>W</span>elcome <span>t</span>o <span>S</span>howTables</h1>
+            <h3 className="text3">Start with us</h3>
+        </div>
+        <div className="button-container">
+            <button className="btn-tables">Show Tables</button>
+            <button className="btn-emails">Emails Data </button>
+        </div>
       </div>
     </div>
   )
