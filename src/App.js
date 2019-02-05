f82b60654ecabf415c2eadb741be01714c3e7d9b
shymaSane <shymaSane1@gmail.com>
2019-02-05 13:07:17 +0200

    cover image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Home from './components/layouts/Home'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Home />
       </div>
     );
   }
--- src/components/layouts/Home.js
+++ src/components/layouts/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
+
   return (
-    <div>
+    <div className="container">
+      <div className="cover-text">
+        <h4>Inspire your data</h4>
+        <h1>Welcome to ShowTables</h1>
+        <h3>Start with us</h3>
       
+      </div>
     </div>
   )
 }
 
-export default Home()
+export default Home
