2d914974f66e1c189a85814f2b547eaf93daa30c
shymaSane <shymaSane1@gmail.com>
2019-02-05 18:23:08 +0200

    setup context api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import {Provider} from './Context';
+import CSVReader from 'react-csv-reader'
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/layouts/Home'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Provider>
+        <div className="App">
+          <Home />
+        </div>
+      </Provider>
     );
   }
 }
