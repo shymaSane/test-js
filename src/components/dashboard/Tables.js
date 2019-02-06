b99ec951bde6fdcdbc4fd1d7b2be9865e110f65e
shymaSane <shymaSane1@gmail.com>
2019-02-06 14:28:51 +0200

    choosing columns modal

--- src/components/dashboard/Tables.js
+++ src/components/dashboard/Tables.js
@@ -7,66 +7,22 @@ class Tables extends Component {
   render() {
     return (
         <Consumer>
+            
             {value => {
                 const {tablesData} = value;
-                const columns = [{
-                    Header:'Address',
-                    accessor: 'address',
-                    sortable: false,
-                    filterable: false
-                    }, {
-                    Header:'City',
-                    accessor: 'city',
-                    sortable: false 
-                    }, {
-                    Header:'Company',
-                    accessor: 'company_name',
-                    sortable: false,
-                    filterable: false
-                    }, {
-                    Header:'Email',
-                    accessor: 'email',
-                    sortable: false,
-                    filterable: false  
-                    }, {
-                    Header:'First Name',
-                    accessor: 'first_name'
-                    }, {
-                    Header:'Last Name',
-                    accessor: 'last_name'  
-                    }, {
-                    Header:'Phone1',
-                    accessor: 'phone1',
-                    sortable: false,
-                    filterable: false
-                    }, {
-                    Header:'Phone2',
-                    accessor: 'phone2',
-                    sortable: false,
-                    filterable: false
-                    }, {
-                    Header:'Postal',
-                    accessor: 'postal',
-                    sortable: false,
-                    filterable: false 
-                    }, {
-                    Header:'Province',
-                    accessor: 'province'
-                    }, {
-                    Header:'Website',
-                    accessor: 'web' ,
-                    sortable: false,
-                    filterable: false
-                    }
-                ]
+                const {columns} = value
                 return(
-                   <ReactTable
-                    data={tablesData}
-                    columns={columns}
-                    filterable
-                   >
+                    <div>
+                        <div>hello</div>
+                        <ReactTable
+                            data={tablesData}
+                            columns={columns}
+                            filterable
+                            defaultPageSize={15}
+                        >
 
-                   </ReactTable>
+                        </ReactTable>
+                   </div>
                 )
             }}
         </Consumer>
--- src/components/layouts/Home.js
+++ src/components/layouts/Home.js
@@ -1,25 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Home.css';
 import {Link} from 'react-router-dom';
 
+class Home extends Component {
 
-function Home() {
+  state={
+    emails: "",
+    province:""
+  }
 
-  return (
-    <div className="container">
-      <div className="cover-text">
-        <div className="text-container">
-            <h4 className="text1">Inspire your data</h4>
-            <h1 className="text2"><span>W</span>elcome <span>t</span>o <span>S</span>howTables</h1>
-            <h3 className="text3">Start with us</h3>
+  showModal = () =>{
+    let modal = document.getElementById('tables-modal')
+    modal.style.display = "block";
+  }
+
+  closeModal = () =>{
+    let modal = document.getElementById('tables-modal')
+    modal.style.display = "none";
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <div className="cover-text">
+          <div className="text-container">
+              <h4 className="text1">Inspire your data</h4>
+              <h1 className="text2"><span>W</span>elcome <span>t</span>o <span>S</span>howTables</h1>
+              <h3 className="text3">Start with us</h3>
+          </div>
+          <div className="button-container">
+              <button className="btn-tables" onClick={this.showModal}>Show Tables</button>
+              <button className="btn-emails"><Link to='/api/emails' className="nav-link">Emails Data</Link> </button>
+          </div>
         </div>
-        <div className="button-container">
-            <button className="btn-tables"><Link to='/api/tables' className="nav-link">Show Tables</Link></button>
-            <button className="btn-emails"><Link to='/api/emails' className="nav-link">Emails Data</Link> </button>
+        <div id="tables-modal" className="modal">
+          <div className="modal-content">
+            <h2 className="modal-title">Select columns order <span class="close" onClick={this.closeModal}>&times;</span></h2>
+            
+            <form>
+              <label className="columns-label" onSubmit={this.handleSubmit}>
+              Emails column:
+                <select className="select-box" name ="emails" value={this.state.value} onChange={this.handleChange}>
+                  <option value="1"> 1 </option>
+                  <option value="2"> 2 </option>
+                  <option value="3"> 3 </option>
+                  <option value="4"> 4 </option>
+                  <option value="5"> 5 </option>
+                  <option value="6"> 6 </option>
+                  <option value="7"> 7 </option>
+                  <option value="8"> 8 </option>
+                  <option value="9"> 9 </option>
+                  <option value="10"> 10 </option>
+                  <option value="11"> 11 </option>
+                </select>
+              </label>
+              <label onSubmit={this.handleSubmit} className="columns-label">
+              Province column:
+                <select className="select-box" name ="province" value={this.state.value} onChange={this.handleChange}>
+                  <option value="1"> 1 </option>
+                  <option value="2"> 2 </option>
+                  <option value="3"> 3 </option>
+                  <option value="4"> 4 </option>
+                  <option value="5"> 5 </option>
+                  <option value="6"> 6 </option>
+                  <option value="7"> 7 </option>
+                  <option value="8"> 8 </option>
+                  <option value="9"> 9 </option>
+                  <option value="10"> 10 </option>
+                  <option value="11"> 11 </option>
+                </select>
+              </label>
+              <div className="button-warrper"><input type="submit" value="OK" className="ok-button" /></div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  )
+
+    )
+  }
+  
 }
 
 export default Home
