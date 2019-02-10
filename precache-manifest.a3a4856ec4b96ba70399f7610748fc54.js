6c2225490719d0dae1a60e57c8229eb7582d9043
shymaSane <shymaSane1@gmail.com>
2019-02-10 13:39:04 +0200

    Updates

---
+++ precache-manifest.a3a4856ec4b96ba70399f7610748fc54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d48dc2f1776cac4c9b5",
+    "url": "/test-js/static/css/main.425a4ae3.chunk.css"
+  },
+  {
+    "revision": "0d48dc2f1776cac4c9b5",
+    "url": "/test-js/static/js/main.0d48dc2f.chunk.js"
+  },
+  {
+    "revision": "27873035e8c1cb41ff88",
+    "url": "/test-js/static/css/1.a7fab178.chunk.css"
+  },
+  {
+    "revision": "27873035e8c1cb41ff88",
+    "url": "/test-js/static/js/1.27873035.chunk.js"
+  },
+  {
+    "revision": "8334b05c364e56851ca4",
+    "url": "/test-js/static/js/runtime~main.8334b05c.js"
+  },
+  {
+    "revision": "73ddd335126c51440cf6eb802e0e1e71",
+    "url": "/test-js/static/media/cover2.73ddd335.jpg"
+  },
+  {
+    "revision": "3e6edf130e9941c9bfbee69d787e84ef",
+    "url": "/test-js/static/media/cover1.3e6edf13.jpg"
+  },
+  {
+    "revision": "191a62d7615a12d65e4cb1682d6b7039",
+    "url": "/test-js/index.html"
+  }
+];
